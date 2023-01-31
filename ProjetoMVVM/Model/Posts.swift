bd297129b93096b59e862c33b837a8967dbe93e2
ItaloRossySantiago <italocurty24@gmail.com>
2023-01-31 19:44:27 -0300

    Adicionados novos arquivos relacionados a collection view de posts / na HomeScreen foi registrada nova cell criada para o PostCard, na HomeVC feita a inclusão da nova celular e chamado seu setup passando o setlist Criado na viewModel / criada a variável que recebe o array  de posts na viewModel/

---
+++ ProjetoMVVM/Model/Posts.swift
@@ -0,0 +1,14 @@
+//
+//  posts.swift
+//  ProjetoMVVM
+//
+//  Created by Italo Rossy on 30/01/23.
+//
+
+import Foundation
+
+struct Posts {
+    let profileImage:String
+    let userName: String
+    let postImage: String
+}
